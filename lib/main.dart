366a6444c8121fde65983447a71ebbe162d4f0dd
PGioS <pier.giovanni.sedia@gmail.com>
2023-02-05 13:29:52 +0100

    first commit: app with only two answers and three button that can be clicked. Once an answer is clicked the question changes. Only three files at the moment: main.dart, answer.dart, question.dart

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex +1;
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite colour?",
+      "What's your favourite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("MyFirstApp"),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+          ],
+        ),
+      ),
+    );
+  }
+}

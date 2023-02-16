8dcdd346592275c4c259d8274b67b90e2788e92a
PGioS <pier.giovanni.sedia@gmail.com>
2023-02-16 01:37:30 +0100

    implemented a Restart button at the end that makes you restart the quiz. To do that, a new function in the main.dart file has been implemented that then is passed to the Result widget that will run the function once that the button is pressed

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final VoidCallback resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
@@ -22,10 +23,15 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(child: Text('Restart the Quiz'), onPressed: resetHandler,)
+        ],
       ),
     );
   }
